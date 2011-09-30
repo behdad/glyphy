01a6d4e692643a767882ed6b2e7f9b02ac1f23c2
Behdad Esfahbod <behdad@behdad.org>
2011-09-30 14:44:09 -0400

    Merge more code from Maysum
    Lots of cleanup in order now!

--- arc.cc
+++ arc.cc
@@ -324,7 +324,7 @@ arc_bezier_error_improved (const bezier_t &b)
  *        for much cleaner code!!           *
  ********************************************/
 static double 
-binary_find_cut_L (const bezier_t &b,
+binary_find_cut_L_old (const bezier_t &b,
                    double i,
                    double epsilon)
 {
@@ -394,7 +394,7 @@ binary_find_cut_L (const bezier_t &b,
 
 /********** NOT CURRENTLY USED. *************/
 static double 
-binary_find_cut_R (const bezier_t &b,
+binary_find_cut_R_old (const bezier_t &b,
                    double j,
                    double epsilon)
 {
@@ -460,6 +460,83 @@ binary_find_cut_R (const bezier_t &b,
  
 
 
+/*******************************************************************************************************************/
+
+
+
+
+static double 
+binary_find_cut_L (const bezier_t &b,
+                   double i,
+                   double epsilon)
+{
+  double low, high, cut_point, error;  
+  error = arc_bezier_error_improved (b.segment (i, 1));
+
+  /* If error < epsilon, return 1. */
+  if (error < epsilon)
+		return 1.0;
+
+  /* Use binary search to find a good cut_point such that error(bezier cut at cut_point) ~= epsilon. */
+  low = i;
+  high = 1.0;
+  
+  /* Perform [MAX_ITERS] steps of a binary search. */
+  int count;
+  for (count = 1; count <= MAX_ITERS; count++) {
+
+ 	  cut_point = (low + high) / 2.0;
+  	error = arc_bezier_error_improved (b.segment (i, cut_point));
+
+    if (error == epsilon)
+      return cut_point;
+    if (error < epsilon)
+			low = cut_point;
+    else
+      high = cut_point;
+  }
+
+	/* By now, mid should be close enough to the desired value. 
+   * NOTE: We ***might*** be slightly above epsilon... */
+	return cut_point;    
+}
+
+
+static double 
+binary_find_cut_R (const bezier_t &b,
+                   double j,
+                   double epsilon)
+{
+  double low, high, cut_point, error;  
+  error = arc_bezier_error_improved (b.segment (0.0, j));
+
+  /* If error < epsilon, return 1. */
+  if (error < epsilon)
+		return 0.0;
+
+  /* Use binary search to find a good cut_point such that error(bezier cut at cut_point) ~= epsilon. */
+  low = 0.0;
+  high = j;
+  
+  /* Perform [MAX_ITERS] steps of a binary search. */
+  int count;
+  for (count = 1; count <= MAX_ITERS; count++) {
+
+ 	  cut_point = (low + high) / 2.0;
+  	error = arc_bezier_error_improved (b.segment (cut_point, j));
+
+    if (error == epsilon)
+      return cut_point;
+    if (error < epsilon)
+			high = cut_point;
+    else
+      low = cut_point;
+  }
+
+	/* By now, mid should be close enough to the desired value. 
+   * NOTE: We ***might*** be slightly above epsilon... */
+	return cut_point;    
+}
 
 
 
@@ -576,6 +653,260 @@ demo_curve (cairo_t *cr)
 	  }
 
 	  if (1) {
+			/* Test binary cut. */
+			deque<double> left_cuts, right_cuts;	
+			find_cut_points_L (b, EPSILON, left_cuts);
+			find_cut_points_R (b, EPSILON, right_cuts);
+			left_cuts.pop_back ();
+			right_cuts.pop_front ();
+			
+
+			int num_cut_ranges = left_cuts.size ();
+			double cut_low [num_cut_ranges];
+			double cut_high [num_cut_ranges];
+			double cut_point [num_cut_ranges + 2];
+			double arc_error [num_cut_ranges + 1];
+
+			int cut_count = 0;
+			cut_point [0] = 0;
+			while (!left_cuts.empty()) {
+				cut_low [cut_count] = right_cuts.front();
+				cut_high [cut_count] = left_cuts.front();
+				cut_point [cut_count + 1] = (cut_low [cut_count] + cut_high [cut_count]) / 2.0;
+				arc_error [cut_count] = arc_bezier_error_improved (b.segment (cut_point [cut_count], cut_point [cut_count + 1]));
+				printf("Cut range: [%g (%g) %g] ~ %g\n", cut_low [cut_count], cut_point [cut_count + 1], cut_high [cut_count], arc_error [cut_count]); 
+				right_cuts.pop_front();
+				left_cuts.pop_front();
+				cut_count++;
+			}
+			cut_point [cut_count + 1] = 1;
+			arc_error [cut_count] = arc_bezier_error_improved (b.segment (cut_point [cut_count], cut_point [cut_count + 1]));
+
+			/* ..."Jiggle"...    -_-;;      */
+			for (int jiggle_count = 1; jiggle_count < 10; jiggle_count++) {
+				for (int cut_number = 0; cut_number < num_cut_ranges; cut_number++) {
+
+					/* Step size is something like, |error[cn + 1] - error[cn]| / (2^(1+curvature[cut[cn+1]]) * epsilon. */
+					vector_t prime = b.tangent (cut_point [cut_number]);
+					vector_t prime2 = b.d_tangent (cut_point [cut_number]);
+					double len = prime.len();
+		      double curvature = (prime2.dy * prime.dx - prime2.dx * prime.dy) / (len*len*len);
+					double step_size = fabs (arc_error [cut_number+1] - arc_error [cut_number]) / (pow (2, (1+curvature)) * EPSILON);
+					if (arc_error [cut_number+1] > arc_error [cut_number])
+						cut_point [cut_number+1] -= step_size * (cut_point [cut_number+1] - cut_low[cut_number]);
+					else
+						cut_point [cut_number+1] += step_size * (cut_high[cut_number] - cut_point [cut_number+1]);
+
+					/* Update arc errors. */
+					arc_error [cut_number] = arc_bezier_error_improved (b.segment (cut_point [cut_number], cut_point [cut_number + 1]));
+				}
+			}
+
+			for (int cut_number = 0; cut_number < num_cut_ranges; cut_number++) {
+				printf("Cut range: [%g (%g) %g] ~ %g\n", cut_low [cut_number], cut_point [cut_number + 1], cut_high [cut_number], arc_error [cut_number]); 
+			}
+
+
+
+			/* Draw the arcs. */
+			double previous_cut = 0.0;
+			double current_cut;
+			for (cut_count = 0; cut_count <  num_cut_ranges + 1; cut_count++) {				
+				current_cut = cut_point [cut_count + 1];
+				printf(">> Beginning a new arc segment: %g to %g.\n", previous_cut, current_cut);
+
+				circle_t cm (b.point(previous_cut), b.point((previous_cut + current_cut) / 2.0), b.point(current_cut));
+				bezier_t small_b = b.segment(previous_cut, current_cut);
+		
+				double t;
+				for (t = 0; t <= 1; t += .01) {
+				  point_t p = small_b.point (t);
+
+				  /* Draw a line from the curve to the centre of the circle. */
+				  cairo_set_source_rgb (cr, 0, 0, 1);
+				  cairo_move_to (cr, p.x, p.y);
+				  cairo_line_to (cr, cm.c.x, cm.c.y);
+
+				  cairo_stroke (cr);
+				}
+				previous_cut = current_cut;
+
+
+
+				/* divide the curve into two */
+			  Pair<bezier_t> pair = small_b.halve ();
+			  point_t m = pair.second.p0;
+
+			  circle_t c (small_b.p0, m, small_b.p3);
+
+			  double e0 = arc_bezier_error (pair.first, c);
+			  double e1 = arc_bezier_error (pair.second, c);
+			  double e = MAX (e0, e1);
+
+			  printf   ("Estim. arc max error %g\n", e);
+
+			  {
+			    double t;
+			    double e = 0;
+			    for (t = 0; t <= 1; t += .001) {
+						point_t p = small_b.point (t);
+						e = MAX (e, fabs ((c.c - p).len () - c.r));
+			    }
+			    printf ("Actual arc max error %g\n", e);
+			  }
+
+				cairo_save (cr);
+			  cairo_set_source_rgba (cr, 0.0, 1.0, 0.0, 1.0);
+
+			  cairo_set_line_cap (cr, CAIRO_LINE_CAP_ROUND);
+			  cairo_move_to (cr, small_b.p0.x, small_b.p0.y);
+			  cairo_rel_line_to (cr, 0, 0);
+			  cairo_set_line_width (cr, line_width * 2);
+			  cairo_stroke (cr);
+
+			  cairo_set_line_width (cr, line_width * 0.5);
+			  {
+			    arc_t a (small_b.p0, small_b.p3, m, true);
+			    circle_t c  = a.circle ();
+
+			    double a0 = (a.p0 - c.c).angle ();
+			    double a1 = (a.p1 - c.c).angle ();
+			    printf("Arc from %g to %g.\n", a0, a1);
+
+			    if (a0 < a1)
+			      cairo_arc (cr, c.c.x, c.c.y, c.r, a0, a1);
+			    else
+			      cairo_arc_negative (cr, c.c.x, c.c.y, c.r, a0, a1);
+			  }
+			  cairo_stroke (cr);
+
+			  cairo_restore (cr);
+
+			}
+
+
+
+			/* Make arrays storing cut bounds, cut positions, and error values. */
+/*			double cut_low [right_cuts.size()];
+			double cut_high [left_cuts.size()];
+			double cut_place [left_cuts.size() + 2];
+			double arc_error [left_cuts.size() + 1];
+
+			int cut_index = 0;
+
+			cut_place [0] = 0;
+			cut_place [left_cuts.size() + 1] = 1.0;
+			printf ("The cut ranges are as follows:\n");
+			while (!left_cuts.empty())
+ 		  {
+				cut_low [cut_index] = right_cuts.front();
+				cut_high [cut_index] = left_cuts.front();
+				cut_place [cut_index + 1] = cut_high [cut_index]; // (cut_low [cut_index] + cut_high [cut_index]) / 2.0;
+				arc_error [cut_index] = arc_bezier_error_improved (b.segment (cut_place[cut_index], cut_place [cut_index + 1]));
+
+   		  printf("[%g (%g) %g] ~ %g\n", cut_low [cut_index], cut_place[cut_index + 1], cut_high[cut_index], arc_error[cut_index]);
+				
+   		  left_cuts.pop_front();
+			 	right_cuts.pop_front();
+
+				cut_index++;
+  		}  
+			printf("\n"); */
+
+			
+
+
+
+	    
+
+	    
+	  }
+
+	  {
+	    for (double t = 0; t <= 1; t += .05)
+	    {
+	      point_t p = b.point (t);
+	      circle_t cv = b.osculating_circle (t);
+	      cairo_move_to (cr, p.x, p.y);
+	      cairo_line_to (cr, cv.c.x, cv.c.y);
+	    }
+	  }
+	}
+	current_point = data[3];
+	break;
+    case CAIRO_PATH_CLOSE_PATH:
+	break;
+    default:
+	g_assert_not_reached ();
+    }
+  }
+  cairo_set_source_rgb (cr, 0, 0, 0);
+  cairo_stroke (cr);
+  cairo_restore (cr);
+
+#if 0
+  cairo_save (cr);
+  for (i=0; i < path->num_data; i += path->data[i].header.length) {
+    data = &path->data[i];
+    switch (data->header.type) {
+    case CAIRO_PATH_MOVE_TO:
+	cairo_move_to (cr, data[1].point.x, data[1].point.y);
+	break;
+    case CAIRO_PATH_LINE_TO:
+	cairo_line_to (cr, data[1].point.x, data[1].point.y);
+	break;
+    case CAIRO_PATH_CURVE_TO:
+	cairo_curve_to (cr, data[1].point.x, data[1].point.y,
+			    data[2].point.x, data[2].point.y,
+			    data[3].point.x, data[3].point.y);
+	break;
+    case CAIRO_PATH_CLOSE_PATH:
+	cairo_close_path (cr);
+	break;
+    default:
+	g_assert_not_reached ();
+    }
+  }
+  cairo_stroke (cr);
+  cairo_restore (cr);
+#endif
+  cairo_path_destroy (path);
+}
+
+
+
+static void
+demo_curve_good (cairo_t *cr)
+{
+  int i;
+  double line_width;
+  cairo_path_t *path;
+  cairo_path_data_t *data, current_point;
+
+  fancy_cairo_stroke_preserve (cr);
+  path = cairo_copy_path (cr);
+  cairo_new_path (cr);
+
+  print_path_stats (path);
+
+  cairo_save (cr);
+  line_width = cairo_get_line_width (cr);
+  cairo_set_line_width (cr, line_width / 16);
+  for (i=0; i < path->num_data; i += path->data[i].header.length) {
+    data = &path->data[i];
+    switch (data->header.type) {
+    case CAIRO_PATH_MOVE_TO:
+    case CAIRO_PATH_LINE_TO:
+	current_point = data[1];
+	break;
+    case CAIRO_PATH_CURVE_TO:
+	{
+#define P(d) (point_t (d.point.x, d.point.y))
+	  bezier_t b (P (current_point), P (data[1]), P (data[2]), P (data[3]));
+#undef P
+
+	  if (1)
+	  {
 	      /* Test binary cut. */
 	      deque<double> left_cuts, right_cuts;		
 	      find_cut_points_L (b, EPSILON, left_cuts);
