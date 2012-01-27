ac83c24a025aacb01247d4670e45765ab90b850c
Behdad Esfahbod <behdad@behdad.org>
2012-01-27 15:31:59 -0500

    Some vectorizing love

--- demo/glyphy-demo-atlas.glsl
+++ demo/glyphy-demo-atlas.glsl
@@ -1,11 +1,10 @@
-int mod (const int a, const int b) { return a - (a / b) * b; }
-int div (const int a, const int b) { return a / b; }
-
 vec4
 glyphy_texture1D_func (sampler2D tex, vec4 atlas_info, vec4 atlas_pos, int offset)
 {
   vec2 orig = atlas_pos.xy;
-  ivec4 atlas = ivec4 (atlas_info);
-  return texture2D (tex, vec2 ((orig.x + mod (offset, atlas.z) + .5) / float (atlas.x),
-			       (orig.y + div (offset, atlas.z) + .5) / float (atlas.y)));
+  int glyph_width = int (atlas_info.z);
+  vec2 pos = (atlas_pos.xy +
+	      vec2 (mod (offset, glyph_width), offset / glyph_width) +
+	      vec2 (.5, .5)) / atlas_info.xy;
+  return texture2D (tex, pos);
 }
--- demo/glyphy-demo-fshader.glsl
+++ demo/glyphy-demo-fshader.glsl
@@ -3,40 +3,46 @@ uniform vec4 u_atlas_info;
 
 varying vec4 v_glyph;
 
+vec3 glyph_decode_f (vec4 v)
+{
+  ivec2 glyph_layout_ivec = ivec2 (mod (v_glyph.zw, 256));
+  int glyph_layout = glyph_layout_ivec.x * 256 + glyph_layout_ivec.y;
+  vec2 atlas_pos = vec2 (ivec2 (v_glyph.zw) / 256 * 4);
+  return vec3 (atlas_pos, glyph_layout);
+}
+
 void main()
 {
   vec2 p = v_glyph.xy;
-  int glyph_layout = mod (int (v_glyph.z), 256) * 256 + mod (int (v_glyph.w), 256);
-  vec4 atlas_pos = vec4 (int (v_glyph.z) / 256, int (v_glyph.w) / 256, 0, 0) * 4;
+  vec3 decoded = glyph_decode_f (v_glyph);
+  vec4 atlas_pos = vec4 (decoded.xy, 0, 0);
+  int glyph_layout = int (decoded.z);
 
   /* isotropic antialiasing */
-  float m = length (vec2 (float (dFdx (p)),
-			  float (dFdy (p))));
-  //float m = float (fwidth (p)); //for broken dFdx/dFdy
+  vec2 dpdx = dFdx (p);
+  vec2 dpdy = dFdy (p);
+  float m = max (length (dpdx), length (dpdy));
 
   float sdist = glyphy_sdf (p, u_atlas_tex, u_atlas_info, atlas_pos, glyph_layout);
   float udist = abs (sdist);
 
-  vec4 color = vec4(0,0,0,1);
+  vec4 color = vec4 (0,0,0,1);
 
   // Color the outline red
-  color += vec4(1,0,0,0) * smoothstep (2 * m, 0, udist);
-
+  color += vec4 (1,0,0,0) * smoothstep (2 * m, 0, udist);
   // Color the distance field in green
-  color += vec4(0,1,0,0) * ((1 + sin (sdist / m))) * sin (pow (udist, .8) * 3.14159265358979) * .5;
+  color += vec4 (0,1,0,0) * ((1 + sin (sdist / m))) * sin (pow (udist, .8) * 3.14159265358979) * .5;
+  // Color the inside of the glyph a light red
+  color += vec4 (.5,0,0,0) * smoothstep (m, -m, sdist);
 
 /*
   // Color points green
-  color = mix(vec4(0,1,0,1), color, smoothstep (2 * m, 3 * m, min_point_dist));
+  color = mix (vec4 (0,1,0,1), color, smoothstep (2 * m, 3 * m, min_point_dist));
 
   // Color the number of endpoints per cell blue
-  color += vec4(0,0,1,0) * num_endpoints * 16./255.;
+  color += vec4 (0,0,1,0) * num_endpoints * 16./255.;
 */
 
-  // Color the inside of the glyph a light red
-  color += vec4(.5,0,0,0) * smoothstep (m, -m, sdist);
-
-//  color = vec4(1,1,1,1) * smoothstep (-m, m, sdist);
-
+//  color = vec4 (1,1,1,1) * smoothstep (-m, m, sdist);
   gl_FragColor = color;
 }
