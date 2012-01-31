0698ab4133ea1809fa3f54961a01361f9a4781d4
Behdad Esfahbod <behdad@behdad.org>
2012-01-30 22:34:21 -0500

    Move more code around

--- src/glyphy-rgba.cc
+++ src/glyphy-rgba.cc
@@ -23,7 +23,6 @@
 #include <glyphy.h>
 
 #include "glyphy-geometry.hh"
-#include "glyphy-rgba.hh"
 
 #include <string.h>
 #include <math.h>
@@ -36,8 +35,79 @@
 #define GRID_H GRID_SIZE
 
 using namespace GLyphy::Geometry;
-using namespace GLyphy::RGBA;
 
+
+#define UPPER_BITS(v,bits,total_bits) ((v) >> ((total_bits) - (bits)))
+#define LOWER_BITS(v,bits,total_bits) ((v) & ((1 << (bits)) - 1))
+#define MIDDLE_BITS(v,bits,upper_bound,total_bits) (UPPER_BITS (LOWER_BITS (v, upper_bound, total_bits), bits, upper_bound))
+
+
+static inline glyphy_rgba_t
+arc_encode (double x, double y, double d)
+{
+  glyphy_rgba_t v;
+
+  /* 12 bits for each of x and y, 8 bits for d */
+  unsigned int ix, iy, id;
+  ix = lround (x * 4095);
+  assert (ix < 4096);
+  iy = lround (y * 4095);
+  assert (iy < 4096);
+#define GLYPHY_MAX_D .5
+  if (isinf (d))
+    id = 0;
+  else {
+    assert (fabs (d) < GLYPHY_MAX_D);
+    id = lround (d * 127. / GLYPHY_MAX_D + 128);
+  }
+  assert (id < 256);
+
+  v.r = id;
+  v.g = LOWER_BITS (ix, 8, 12);
+  v.b = LOWER_BITS (iy, 8, 12);
+  v.a = ((ix >> 8) << 4) | (iy >> 8);
+  return v;
+}
+
+static inline glyphy_rgba_t
+arc_list_encode (unsigned int offset, unsigned int num_points, bool is_inside)
+{
+  glyphy_rgba_t v;
+  v.b = 0; // unused for arc-list encoding
+  v.g = UPPER_BITS (offset, 8, 16);
+  v.b = LOWER_BITS (offset, 8, 16);
+  v.a = LOWER_BITS (num_points, 8, 8);
+  if (is_inside && !num_points)
+    v.a = 255;
+  return v;
+}
+
+static inline glyphy_rgba_t
+line_encode (const Line &line)
+{
+  Line l = line.normalized ();
+  double angle = l.n.angle ();
+  double distance = l.c;
+
+  int ia = -angle / M_PI * 0x8000;
+  if (ia == 0x8000) ia--;
+  unsigned int ua = ia + 0x8000;
+  assert (0 == (ua & ~0xFFFF));
+
+  int id = distance * 0x2000;
+  unsigned int ud = id + 0x4000;
+  assert (0 == (ud & ~0x7FFF));
+
+  /* Marker for line-encoded */
+  ud |= 0x8000;
+
+  glyphy_rgba_t v;
+  v.r = ud >> 8;
+  v.g = ud & 0xFF;
+  v.b = ua >> 8;
+  v.a = ua & 0xFF;
+  return v;
+}
 
 static unsigned int /* 16bits */
 glyph_layout_encode (unsigned int grid_w,
@@ -48,6 +118,7 @@ glyph_layout_encode (unsigned int grid_w,
 
   return (grid_w << 8) | grid_h;
 }
+
 
 /* Given a cell, fills the vector closest_arcs with arcs that may be closest to some point in the cell.
  * Uses idea that all close arcs to cell must be ~close to center of cell.
