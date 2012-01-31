15fa9aa4b2ebd887579bdb89e6e3297cbc0f667f
Behdad Esfahbod <behdad@behdad.org>
2012-01-30 20:09:14 -0500

    Move code around

--- demo/glyphy-demo.cc
+++ demo/glyphy-demo.cc
@@ -23,13 +23,13 @@
 #include "demo-common.h"
 #include "demo-buffer.h"
 #include "demo-font.h"
-#include "demo-shader.h"
 #include "demo-state.h"
 
-demo_state_t st;
-demo_buffer_t *buffer;
-
-#include "glyphy-demo-glut.h"
+#if defined(__APPLE__)
+    #include <Glut/glut.h>
+#else
+    #include <GL/glut.h>
+#endif
 
 
 static FT_Face
@@ -46,11 +46,199 @@ open_ft_face (const char   *font_path,
   return face;
 }
 
+
+demo_state_t st;
+demo_buffer_t *buffer;
+
+/* Animate */
+
+#include <sys/time.h>
+
+static int num_frames = 0;
+static int animate = 0;
+static long fps_start_time = 0;
+static long last_time = 0;
+static double phase = 0;
+
+
+#define WINDOW_SIZE 700
+
+
+
+/* return current time in milli-seconds */
+static long
+current_time (void)
+{
+   struct timeval tv;
+   struct timezone tz;
+   (void) gettimeofday(&tv, &tz);
+   return (long) tv.tv_sec * 1000 + (long) tv.tv_usec / 1000;
+}
+
+
+static void
+timed_step (int ms)
+{
+  if (animate) {
+    glutTimerFunc (ms, timed_step, ms);
+    num_frames++;
+    glutPostRedisplay ();
+  }
+}
+
+static void
+idle_step (void)
+{
+  if (animate) {
+    glutIdleFunc (idle_step);
+    num_frames++;
+    glutPostRedisplay ();
+  }
+}
+
+static bool has_fps_timer = false;
+
+static void
+print_fps (int ms)
+{
+  if (animate) {
+    glutTimerFunc (ms, print_fps, ms);
+    long t = current_time ();
+    printf ("%gfps\n", num_frames * 1000. / (t - fps_start_time));
+    num_frames = 0;
+    fps_start_time = t;
+  } else
+    has_fps_timer = false;
+}
+
+static void
+start_animation (void)
+{
+  num_frames = 0;
+  fps_start_time = current_time ();
+  //glutTimerFunc (1000/60, timed_step, 1000/60);
+  glutIdleFunc (idle_step);
+  if (!has_fps_timer) {
+    has_fps_timer = true;
+    glutTimerFunc (5000, print_fps, 5000);
+  }
+}
+
+static void
+toggle_animation (void)
+{
+  last_time = 0;
+  animate = !animate;
+  if (animate)
+    start_animation ();
+}
+
+void
+reshape_func (int width, int height)
+{
+  glViewport (0, 0, width, height);
+  glutPostRedisplay ();
+}
+
+static void
+set_uniform (const char *name, double *p, double value)
+{
+  *p = value;
+  glUniform1f (glGetUniformLocation (st.program, name), value);
+  printf ("Setting %s to %g\n", name, value);
+  glutPostRedisplay ();
+}
+#define SET_UNIFORM(name, value) set_uniform (#name, &st.name, value)
+
+
+static void
+keyboard_func (unsigned char key, int x, int y)
+{
+  switch (key) {
+    case '\033':
+    case 'q':
+      exit (0);
+      break;
+
+    case ' ':
+      toggle_animation ();
+      break;
+
+    case 'f':
+      glutFullScreen ();
+      break;
+
+    case 'd':
+      SET_UNIFORM (u_debug, 1 - st.u_debug);
+      break;
+
+    case 'a':
+      SET_UNIFORM (u_contrast, st.u_contrast / .9);
+      break;
+    case 'z':
+      SET_UNIFORM (u_contrast, st.u_contrast * .9);
+      break;
+
+    case 'g':
+      SET_UNIFORM (u_gamma, st.u_gamma / .9);
+      break;
+    case 'b':
+      SET_UNIFORM (u_gamma, st.u_gamma * .9);
+      break;
+  }
+}
+
+
+static void
+display_func (void)
+{
+  int viewport[4];
+  glGetIntegerv (GL_VIEWPORT, viewport);
+  GLuint width  = viewport[2];
+  GLuint height = viewport[3];
+
+  double elapsed_time = 0;
+  long t = current_time ();
+  if (animate) {
+    if (last_time == 0)
+      last_time = t;
+    elapsed_time = t - last_time;
+    last_time = t;
+  }
+  phase += elapsed_time;
+
+  double theta = M_PI / 360.0 * phase * .05;
+  GLfloat mat[] = { +cos(theta)*2/width, -sin(theta)*2/height, 0., 0.,
+		    -sin(theta)*2/width, -cos(theta)*2/height, 0., 0.,
+			     0.,          0., 0., 0.,
+			     0.,          0., 0., 1., };
+
+  glClearColor (1, 1, 1, 1);
+  glClear (GL_COLOR_BUFFER_BIT);
+
+  glUniformMatrix4fv (glGetUniformLocation (st.program, "u_matViewProjection"), 1, GL_FALSE, mat);
+
+  demo_buffer_draw (buffer, &st);
+  glutSwapBuffers ();
+}
+
 int
 main (int argc, char** argv)
 {
   const char *font_path = NULL;
   const char *text = NULL;
+
+  glutInit (&argc, argv);
+  glutInitWindowSize (WINDOW_SIZE, WINDOW_SIZE);
+  glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+  glutCreateWindow("GLyphy Demo");
+  glutReshapeFunc (reshape_func);
+  glutDisplayFunc (display_func);
+  glutKeyboardFunc (keyboard_func);
+
+  glewInit ();
+//  if (!glewIsSupported ("GL_VERSION_2_0"))
+//    abort ();// die ("OpenGL 2.0 not supported");
 
   if (argc != 3) {
     fprintf (stderr, "Usage: %s FONT_FILE TEXT\n", argv[0]);
@@ -58,8 +246,6 @@ main (int argc, char** argv)
   }
    font_path = argv[1];
    text = argv[2];
-
-  glut_init (&argc, argv);
 
   demo_state_init (&st);
   FT_Face face = open_ft_face (font_path, 0);
@@ -75,6 +261,7 @@ main (int argc, char** argv)
   demo_state_setup (&st);
   glutMainLoop ();
 
+  demo_buffer_destroy (buffer);
   demo_font_destroy (font);
   demo_state_fini (&st);
 
