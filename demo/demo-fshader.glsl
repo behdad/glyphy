d98e4fa3d2967bbb53d675880723cf97691f2dfc
Behdad Esfahbod <behdad@behdad.org>
2012-01-31 21:59:13 -0500

    Some debug color scheme changes now that we have blending

--- demo/demo-fshader.glsl
+++ demo/demo-fshader.glsl
@@ -42,23 +42,26 @@ main()
     alpha = pow (alpha, u_gamma);
     color = vec4 (color.rgb,color.a * alpha);
   } else {
+    color = vec4 (0,0,0,0);
+
     // Color the inside of the glyph a light red
-    color += vec4 (.5,0,0,0) * smoothstep (1, -1, sdist);
+    color += vec4 (.5,0,0,.5) * smoothstep (1, -1, sdist);
 
     float udist = abs (sdist);
     float gudist = abs (gsdist);
     // Color the outline red
-    color += vec4 (1,0,0,0) * smoothstep (2, 0, udist);
+    color += vec4 (1,0,0,1) * smoothstep (2, 1, udist);
     // Color the distance field in green
-    color += vec4 (0,1,0,0) * ((1 + sin (sdist))) * sin (pow (gudist, .8) * 3.14159265358979) * .5;
+    if (!glyphy_isinf (udist))
+      color += vec4 (0,.3,0,(1 + sin (sdist)) * abs(1 - gsdist * 3) / 3.);
 
-    float pdist = glyphy_point_dist (p, gi.glyph_layout GLYPHY_DEMO_EXTRA_ARGS) / m;
+    float pdist = glyphy_point_dist (p, gi.glyph_layout GLYPHY_DEMO_EXTRA_ARGS);
     // Color points green
-    color = mix (vec4 (0,1,0,1), color, smoothstep (2, 3, pdist));
+    color = mix (vec4 (0,1,0,.5), color, smoothstep (.005, .006, pdist));
 
     glyphy_arc_list_t arc_list = glyphy_arc_list (p, gi.glyph_layout GLYPHY_DEMO_EXTRA_ARGS);
     // Color the number of endpoints per cell blue
-    color += vec4 (0,0,1,0) * arc_list.num_endpoints * 32./255.;
+    color += vec4 (0,0,1,.1) * arc_list.num_endpoints * 32./255.;
   }
 
   gl_FragColor = color;
